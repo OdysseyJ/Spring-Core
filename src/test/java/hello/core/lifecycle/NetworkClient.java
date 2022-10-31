407fdd04c100fb313f556763d79c3978dfdd9b22
seongwoon jeong <ggogumalove@gmail.com>
2022-10-31 10:18:08 +0900

    [feat] add post construct and pre distroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,11 +1,15 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
     public NetworkClient(){
         System.out.println("생성자 호출, ulr = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -23,5 +27,16 @@ public class NetworkClient {
 
     public void disconnect(){
         System.out.println("close " + url);
+    }
+
+    @PostConstruct
+    public void init() {
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @PreDestroy
+    public void close() {
+        disconnect();
     }
 }
